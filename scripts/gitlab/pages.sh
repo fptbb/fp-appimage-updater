797e887b20dc1c15f3259dd3e0b6f60fd358718b
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2026-04-07 12:05:14 -0300

    chore: update GitLab Pages redirect rules

--- scripts/gitlab/pages.sh
+++ scripts/gitlab/pages.sh
@@ -6,4 +6,8 @@ root_dir="$(CDPATH='' cd -- "$(dirname -- "$0")/../.." && pwd)"
 mkdir -p "$root_dir/public"
 cp "$root_dir/install.sh" "$root_dir/public/i"
 cp "$root_dir/install-github.sh" "$root_dir/public/ig"
-printf '/ https://gitlab.com/fpsys/fp-appimage-updater 301\n' > "$root_dir/public/_redirects"
+cat > "$root_dir/public/_redirects" <<'EOF'
+/ https://gitlab.com/fpsys/fp-appimage-updater 301
+/:file https://gitlab.com/fpsys/fp-appimage-updater/-/blob/main/:file 301
+/* https://gitlab.com/fpsys/fp-appimage-updater/-/blob/main/:splat 301
+EOF
